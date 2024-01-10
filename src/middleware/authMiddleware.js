ce627c38240199100f5c9e263c047aa01610891d
dixitmanidhakal <dixitmani.dakal@gmail.com>
2024-01-10 18:40:40 +0545

    changes

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -6,6 +6,7 @@ const authenticatedRoute = express.Router();
 authenticatedRoute.use(async function (req, res, next) {
   try {
     let accessTokenFromClient = req.headers.authorization?.split(" ")[1];
+    console.log("token", accessTokenFromClient);
 
     if (!accessTokenFromClient) {
       return res.status(401).send("Authorization Header is missing");
